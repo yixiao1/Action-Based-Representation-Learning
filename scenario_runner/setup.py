71224c62da292cb7486e5aeb4edfd55cf5787439
YiXiao <yixiao@cudahpc04.cvc.uab.es>
2020-08-09 14:10:48 +0200

    upload scenario

---
+++ scenario_runner/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='srunner',
+     version='0.4',
+     scripts=['scenario_runner.py', 'manual_control.py'],
+     author="Carla Team",
+     author_email="carla.simulator@gmail.com",
+     description="The package in order to use scenarios for CARLA",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/carla-simulator/scenario_runner",
+     packages=setuptools.find_packages(),
+     install_requires=[
+        'xmlschema',
+        'py_trees==0.8.3',
+        'six>=1.13.0',
+        'numpy>=1.16',
+         'networkx==2.2',
+         'shapely'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
